075b6890483692550a4d4f6dd2e89eea496b6d45
Fabrice Théoleyre <theoleyre@unistra.fr>
2015-07-03 16:50:23 +0200

    error "nb. of cells" in sixtop

--- software/openvisualizer/openvisualizer/moteConnector/StackDefines.py
+++ software/openvisualizer/openvisualizer/moteConnector/StackDefines.py
@@ -110,8 +110,9 @@ errorDescriptions = {
   64: "not enough space in openqueue for non prioritar packets",
   65: "received frame with invalid CRC",
   66: "invalid IEEE 802.15.4e headers",
-  67: "buffer overflow for output in openserial (indexWrite {0}, IndexRead {1})",
+  67: "serial overflow for output in openserial (indexWrite {0}, location {1})",
   68: "buffer overflow: too many packets (space left {0}, reserved for management {1})",
+  69: "too many cells to reserve in the sixtop request (asked {0}, max {1})",
   69: "generic error {0} {1}",
   70: "unknown error {0} {1}"
 }
